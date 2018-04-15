2b63ba7d21cc72c39b88abdc7592be03f86e2b31
ScarbyDo <Scarbydo@users.noreply.github.com>
2018-04-15 15:02:30 -0400

    Removing useless commands

--- DM_ProjectNoShow.R
+++ DM_ProjectNoShow.R
@@ -1,6 +1,1 @@
-echo "# DataMining1_NoShow" >> README.md
-git init
-git add README.md
-git commit -m "Justin's first commit"
-git remote add origin https://github.com/Scarbydo/DataMining1_NoShow.git
-git push -u origin master
+#  Justin is testing changes
