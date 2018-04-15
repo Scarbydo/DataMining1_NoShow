24390c7beebbaf491bbfec175a01a35976a968f6
ScarbyDo <Scarbydo@users.noreply.github.com>
2018-04-14 21:55:08 -0400

    Add files via upload

---
+++ DM_ProjectNoShow.R
@@ -0,0 +1,6 @@
+echo "# DataMining1_NoShow" >> README.md
+git init
+git add README.md
+git commit -m "Justin's first commit"
+git remote add origin https://github.com/Scarbydo/DataMining1_NoShow.git
+git push -u origin master
