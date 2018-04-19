b830e98bc923d92dfe6b08e8d7087ceabe885e0b
Greg <38381026+grhodes1011@users.noreply.github.com>
2018-04-18 23:25:34 -0400

    train and test sets
    Cleaned up the data a little and split it into training and testing sets

--- DM_ProjectNoShow.R
+++ DM_ProjectNoShow.R
@@ -12,34 +12,51 @@
 ## Main ##
 
 # Import Data
-  library(readr)
-  NoShowData <- read_csv("KaggleV2-May-2016.csv")
-  #View(NoShowData)
-
+library(readr)
+NoShowData <- read_csv("KaggleV2-May-2016.csv")
+#View(NoShowData)
+  
 # Data Exploration
-  typeof(NoShowData)
-  names(NoShowData)
-  str(NoShowData)
-  dim(NoShowData)
-  
-  #check for missing data
-  sum(is.na(NoShowData$PatientId))
-  sum(is.na(NoShowData$AppointmentID))
-  sum(is.na(NoShowData$Gender))
-  sum(is.na(NoShowData$ScheduledDay))
-  sum(is.na(NoShowData$AppointmentDay))
-  sum(is.na(NoShowData$Age))
-  sum(is.na(NoShowData$Neighbourhood))
-  sum(is.na(NoShowData$Scholarship))
-  sum(is.na(NoShowData$Hipertension))
-  sum(is.na(NoShowData$Diabetes))
-  sum(is.na(NoShowData$Alcoholism))
-  sum(is.na(NoShowData$Handcap))
-  sum(is.na(NoShowData$SMS_received))
-  sum(is.na(NoShowData$`No-show`))
-  
-  
-  # examin correlation between variables using pairs()
-  length(NoShowData$PatientId)
-  
+typeof(NoShowData)
+names(NoShowData)
+str(NoShowData)
+dim(NoShowData)
+
+NoShowData$Gender <- as.factor(NoShowData$Gender)
+NoShowData$`No-show` <- as.factor(NoShowData$`No-show`)
+names(NoShowData) <- list("PatientId","AppointmentId","Gender","ScheduledDay","AppointmentDay","Age","Neighborhood","Scholarship","Hypertension","Diabetes","Alcoholism","Handicap","SmsReceived","NoShow")
+
+numUniqueNeighborhoods <- length(unique(NoShowData$Neighborhood))
+# 81 of the 110527 are unique. factorize it?
+
+#check for missing data
+sum(is.na(NoShowData$PatientId))
+sum(is.na(NoShowData$AppointmentId))
+sum(is.na(NoShowData$Gender))
+sum(is.na(NoShowData$ScheduledDay))
+sum(is.na(NoShowData$AppointmentDay))
+sum(is.na(NoShowData$Age))
+sum(is.na(NoShowData$Neighborhood))
+sum(is.na(NoShowData$Scholarship))
+sum(is.na(NoShowData$Hypertension))
+sum(is.na(NoShowData$Diabetes))
+sum(is.na(NoShowData$Alcoholism))
+sum(is.na(NoShowData$Handicap))
+sum(is.na(NoShowData$SmsReceived))
+sum(is.na(NoShowData$NoShow))
+
+trainSize <- floor(0.75 * nrow(NoShowData))
+set.seed(456)
+trainInds <- sample(seq_len(nrow(NoShowData)), size=trainingSetSize)
+train <- NoShowData[trainInds,]
+test <- NoShowData[-trainInds,]
+yColInd <- grep('NoShow', names(NoShowData))
+trainX <- train[-yColInd]
+trainY <- train[yColInd]
+testX <- test[-yColInd]
+testY <- test[yColInd]
+
+# examine correlation between variables using pairs()
+# I don't think we need to check this out, since we're categorical
+
 # Performance measure: use F-Score
